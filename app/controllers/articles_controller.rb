6e044f4c2484bf9603e4aaa132f1b0b25e083920
kei-ebihara <kei.ebihara.hikarituushin@gmail.com>
2019-11-18 22:13:29 +0900

    論文アプリ修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,5 @@
 class ArticlesController < ApplicationController
     def show
-      # Itemモデルのfindメソッドを使用して、パラメータの値に一致するidのデータを検索している
-      # 取得したデータを@itemというインスタンス変数にセットする
       @article = Article.find(params[:id])
     end
 
@@ -13,20 +11,11 @@ class ArticlesController < ApplicationController
       @article = Article.new
     end  
 
-
-
     def create
       @article = Article.new(article_params)
 
-      # 上の行はインスタンスを作っただけ。saveメソッドで保存しないと、消える。
       if @article.save
-
-      # saveの後にshow(商品詳細ページ)に飛ぶ設定をします。
-      # これを設定しないと、プログラムが行き場をなくしてエラーが起きます。
-      redirect_to @article, notice: "論文を登録しました。"
-
-      # redirect_to "/items"
-      # だとindexページにリダイレクトする
+        redirect_to @article, notice: "論文を登録しました。"
       else
         render :new
       end
@@ -47,13 +36,9 @@ class ArticlesController < ApplicationController
       @article.destroy
       redirect_to articles_path
     end
- 
+
     private 
-    # params.require(key).permit(filter)
-    # key
-    #   Strong Parameters を適用したい params の key を指定する。
-    # filter
-    #   Strong Parameters で許可するカラムを指定する。 
+
     def article_params
       params.require(:article).permit(:title, :body)
     end
